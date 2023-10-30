6b58664f66b0b5e996e8fc076853014709d1f6fd
Leonardo Simon Monteiro <135035590+LeoSimonn@users.noreply.github.com>
2023-10-30 22:53:23 +0000

    vetores novos

--- Vetores/Main.java
+++ Vetores/Main.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
-
 import Vetores.OperadorDeVetores;
+import java.util.Arrays;
 
 public class Main {
     
---
+++ VetoresEx2/Main.java
@@ -0,0 +1,26 @@
+package VetoresEx2;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    int[] vetor21 = {1, 2, 3, 4, 5};
+    int[] vetor23 = {1, 2, 3, 4, 5};
+
+    OperarVetores operarVetores2 = new OperarVetores();
+
+    System.out.println(operarVetores2.mediaVetor(vetor21));
+
+        
+        
+        
+        
+
+
+    }
+
+    
+    
+
+}
